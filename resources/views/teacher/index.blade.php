90953cd244bf10e76e85d9cb4d081b98574cdc71
roytamba <roymartogit@gmail.com>
2025-09-10 18:10:32 +0700

    breadcrumb
    for every modules

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -7,19 +7,7 @@
 @section('content')
     <div class="container-fluid">
         {{-- Breadcrumb --}}
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">Teachers</h4>
-                    <div class="page-title-right">
-                        <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Teacher Management</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('layouts.components.breadcrumb')
 
         {{-- Main Content --}}
         <div class="row">
